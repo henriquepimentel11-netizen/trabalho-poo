1f4feb85ea994a37df39aee8bed1b6502c2c886e
Henrique Pimentel <henrique.pimentel11@gmail.com>
2026-06-15 15:09:26 -0300

    Update Missao.java

--- Sistemax/src/database/model/Missao.java
+++ Sistemax/src/database/model/Missao.java
@@ -1,5 +1,10 @@
 package database.model;
 
 public class Missao {
-    
+    private int id;
+    private String titulo;
+    private String descricao;
+    private String rank_missao;
+    private String vila_origem;
+    private String status;
 }
