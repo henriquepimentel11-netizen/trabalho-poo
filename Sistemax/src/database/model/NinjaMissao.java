33e933d8ff55cd6f6684802598ac9f8595254126
Henrique Pimentel <henrique.pimentel11@gmail.com>
2026-06-15 16:03:43 -0300

    NinjaMissao estruturas

---
+++ Sistemax/src/database/model/NinjaMissao.java
@@ -0,0 +1,9 @@
+package database.model;
+
+public class NinjaMissao {
+    private int id;
+    private int id_ninja;
+    private int id_missao;
+    private String funcao;
+    private int data_participacao;
+}
