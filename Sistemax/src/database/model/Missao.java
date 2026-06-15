2ad1379d3bfb91a77947898f4df9cd40a308505b
Henrique Pimentel <henrique.pimentel11@gmail.com>
2026-06-15 15:12:21 -0300

    Getter e Setters Missao

--- Sistemax/src/database/model/Missao.java
+++ Sistemax/src/database/model/Missao.java
@@ -7,4 +7,51 @@ public class Missao {
     private String rank_missao;
     private String vila_origem;
     private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getRank_missao() {
+        return rank_missao;
+    }
+
+    public String getVila_origem() {
+        return vila_origem;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setRank_missao(String rank_missao) {
+        this.rank_missao = rank_missao;
+    }
+
+    public void setVila_origem(String vila_origem) {
+        this.vila_origem = vila_origem;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
